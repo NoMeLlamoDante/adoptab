02c755d9f8878f1dc797353d576c84f659b71b89
NoMeLlamoDante <eo.zm24@outlook.com>
2025-07-05 18:35:21 -0600

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,17 @@
 ssh -o StrictHostKeyChecking=no  $USER@$IP <<SSH
+set -e
 
-cd workplace/adoptab
+cd ~workplace/adoptab
+
+git reset --hard
+
+git clean -fd
 
 git pull --rebase origin main
 
 source env/bin/activate
 
-pip install -r requirements.txt 
+pip install -r requirements.txt
 
 python3 manage.py collectstatic --noinput
 
