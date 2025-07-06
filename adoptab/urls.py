bcc93c862560e4df69f08e8131dc010406202ee3
NoMeLlamoDante <eo.zm24@outlook.com>
2025-07-05 19:22:23 -0600

    fix settings - remove unused module from urls

--- adoptab/urls.py
+++ adoptab/urls.py
@@ -23,7 +23,7 @@ app_name = "core"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name="index.html"), name="index"),
-    path('pictures/', include("pictures.urls")),
+    # path('pictures/', include("pictures.urls")),
     path('pets/', include("pets.urls")),
     path('users/', include("users.urls"))
 ]
