1c2909827b62111bb201f843d9bb7f88b486dc60
NoMeLlamoDante <eo.zm24@outlook.com>
2025-07-06 14:30:17 -0600

    - add profile info to user - add new form for profile info - signal to save data

--- users/views.py
+++ users/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from .forms import CustomUserCreationForm
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
 def register_view(request):
-    form = UserCreationForm(request.POST or None)
+    form = CustomUserCreationForm(request.POST or None)
     if request.method == "POST":
         if form.is_valid():
             user = form.save()
